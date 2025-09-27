5696c911486f422ead2e89e59b99427712732dce
CallMeYudhistira <yudhistira30122008@gmail.com>
2025-09-27 18:52:02 +0700

    feat: column chart at dashboard

--- WEB/app/Http/Controllers/HomeController.php
+++ WEB/app/Http/Controllers/HomeController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    public function __invoke(){
-        return view('home');
+    public function __invoke()
+    {
+        Carbon::setLocale('id');
+
+        $datas = DB::select("SELECT transactions.date, SUM(transactions.total) as total FROM transactions INNER JOIN transaction_details ON transactions.transaction_id = transaction_details.transaction_id GROUP BY transactions.date");
+        $periode = [];
+        $total = [];
+        foreach ($datas as $data) {
+            $periode[] = Carbon::parse($data->date)->translatedFormat('l, d F Y');
+            $total[] = $data->total;
+        }
+        return view('home', compact('periode', 'total'));
     }
 }
--- WEB/resources/views/home.blade.php
+++ WEB/resources/views/home.blade.php
@@ -1,52 +1,87 @@
 @extends('layouts.app')
 @section('title', 'Home')
 @section('content')
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+@section('script')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/5.3.5/apexcharts.min.css"
+        integrity="sha512-IqtQ7LKr3He47p7HjxynmqZfN07VljNkdGyGDdDJ//f1r6bT0IEKQf2CCtSgun/pvbFlNnPDMRrMSQhmSxmSSg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/5.3.5/apexcharts.min.js"
+        integrity="sha512-dC9VWzoPczd9ppMRE/FJohD2fB7ByZ0VVLVCMlOrM2LHqoFFuVGcWch1riUcwKJuhWx8OhPjhJsAHrp4CP4gtw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+@endsection
+<div class="p-4 mt-4 card" style="margin: auto;">
+    <div id="chart"></div>
+</div>
 
-    <div class="p-4 mt-4 card" style="max-height: 500px; max-width: 500px; margin: auto;">
-        <canvas id="myChart"></canvas>
-    </div>
-
-    <script>
-        const ctx = document.getElementById('myChart').getContext('2d');
-        const myChart = new Chart(ctx, {
-            type: 'pie',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        // ... other colors
+<script>
+    var options = {
+        series: [{
+            data: @json($total)
+        }],
+        chart: {
+            height: 350,
+            type: 'bar',
+        },
+        colors: ['#008FFB', '#00E396', '#FEB019', '#FF4560',
+            '#775DD0', '#546E7A', '#26A69A', '#D10CE8'
+        ],
+        plotOptions: {
+            bar: {
+                columnWidth: '45%',
+                distributed: true,
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        legend: {
+            show: false
+        },
+        title: {
+            text: 'Data Penjualan',
+            align: 'center',
+            style: {
+                fontFamily: 'Nata Sans'
+            }
+        },
+        xaxis: {
+            categories: @json($periode),
+            labels: {
+                style: {
+                    colors: ['#008FFB', '#00E396', '#FEB019', '#FF4560',
+                        '#775DD0', '#546E7A', '#26A69A', '#D10CE8'
                     ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        // ... other colors
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
+                    fontSize: '12px',
+                    fontFamily: 'Nata Sans'
                 }
             }
+        },
+        yaxis: {
+            labels: {
+                formatter: function(value) {
+                    return value.toLocaleString('id-ID', {
+                        style: "currency",
+                        currency: "IDR"
+                    });
+                },
+                style: {
+                    fontFamily: 'Nata Sans'
+                }
+            },
+        },
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+</script>
+
+@if ($message = Session::get('error'))
+    <script>
+        Swal.fire({
+            title: "{{ $message }}",
+            icon: "error",
+            draggable: false
         });
     </script>
-
-
-    @if ($message = Session::get('error'))
-        <script>
-            Swal.fire({
-                title: "{{ $message }}",
-                icon: "error",
-                draggable: false
-            });
-        </script>
-    @endif
+@endif
 @endsection
