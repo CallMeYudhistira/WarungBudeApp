b588cae8121bfaac1a986b1e9ef05983f00014ec
CallMeYudhistira <yudhistira30122008@gmail.com>
2025-10-10 16:01:13 +0700

    fix: credit payment for customers

--- WEB/resources/views/kredit/modal/pay.blade.php
+++ WEB/resources/views/kredit/modal/pay.blade.php
@@ -10,31 +10,36 @@
                     @csrf
                     <div class="mb-3">
                         <label class="form-label">Nama Pelanggan</label>
-                        <input type="text" class="form-control" autocomplete="off"
-                            value="{{ $customer->customer_name }}" readonly>
+                        <input type="text" class="form-control" value="{{ $customer->customer_name }}" readonly>
                     </div>
+
                     <div class="mb-3">
-                        <label for="amount_of_debt" class="form-label">Total Hutang</label>
-                        <input type="number" class="form-control" name="amount_of_debt" autocomplete="off"
-                            value="{{ $customer->amount_of_debt }}" readonly id="total">
+                        <label for="amount_of_debt_{{ $customer->customer_id }}" class="form-label">Total Hutang</label>
+                        <input type="number" class="form-control" name="amount_of_debt"
+                            id="total_{{ $customer->customer_id }}" value="{{ $customer->amount_of_debt }}" readonly>
                     </div>
+
                     <div class="mb-3">
-                        <label for="amount_of_paid" class="form-label">Hutang Yang Dibayar</label>
+                        <label for="amount_of_paid_{{ $customer->customer_id }}" class="form-label">Hutang Yang
+                            Dibayar</label>
                         <input type="number" class="form-control" name="amount_of_paid"
-                            placeholder="Hutang yang dibayar..." autocomplete="off" id="bayar"
-                            oninput="sisaHutang(this.value)" min="0">
+                            placeholder="Hutang yang dibayar..." id="bayar_{{ $customer->customer_id }}"
+                            oninput="sisaHutang('{{ $customer->customer_id }}')" min="0">
                     </div>
+
                     <div class="mb-3">
-                        <label for="remaining_debt" class="form-label">Sisa Hutang</label>
-                        <input type="number" class="form-control" name="remaining_debt" autocomplete="off"
-                            id="sisa" readonly value="{{ $customer->amount_of_debt }}" min="0">
+                        <label for="remaining_debt_{{ $customer->customer_id }}" class="form-label">Sisa Hutang</label>
+                        <input type="number" class="form-control" name="remaining_debt"
+                            id="sisa_{{ $customer->customer_id }}" value="{{ $customer->amount_of_debt }}" readonly>
                     </div>
+
                     <div class="mb-3">
-                        <label class="form-label" for="change">Kembalian</label>
-                        <input type="number" class="form-control" autocomplete="off" id="kembali" name="change"
-                            readonly value="0" min="0">
+                        <label for="change_{{ $customer->customer_id }}" class="form-label">Kembalian</label>
+                        <input type="number" class="form-control" name="change"
+                            id="kembali_{{ $customer->customer_id }}" value="0" readonly>
                     </div>
                 </div>
+
                 <div class="modal-footer">
                     <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-success">Bayar</button>
@@ -44,20 +49,20 @@
     </div>
 </div>
 
-    <script>
-        function sisaHutang(bayar) {
-            const total = document.getElementById('total').value;
-            const sisa = document.getElementById('sisa');
-            const kembali = document.getElementById('kembali');
+<script>
+    function sisaHutang(id) {
+        const total = document.getElementById('total_' + id);
+        const bayar = document.getElementById('bayar_' + id);
+        const sisa = document.getElementById('sisa_' + id);
+        const kembali = document.getElementById('kembali_' + id);
 
-            kembali.value = (bayar - total);
-            if (kembali.value < 0) {
-                kembali.value = 0;
-            }
+        const totalVal = parseInt(total.value) || 0;
+        const bayarVal = parseInt(bayar.value) || 0;
 
-            sisa.value = (total - bayar);
-            if (sisa.value < 0) {
-                sisa.value = 0;
-            }
-        }
-    </script>
+        const kembaliVal = bayarVal - totalVal;
+        const sisaVal = totalVal - bayarVal;
+
+        kembali.value = kembaliVal > 0 ? kembaliVal : 0;
+        sisa.value = sisaVal > 0 ? sisaVal : 0;
+    }
+</script>
