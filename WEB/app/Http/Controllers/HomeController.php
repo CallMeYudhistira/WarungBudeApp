673cc472aadba6545b2be726eaa19c299a6506fc
CallMeYudhistira <yudhistira30122008@gmail.com>
2025-10-13 15:21:27 +0700

    fix: profit info at dashboard

--- WEB/app/Http/Controllers/HomeController.php
+++ WEB/app/Http/Controllers/HomeController.php
@@ -13,8 +13,8 @@ class HomeController extends Controller
     {
         #Omset Hari Ini
         $omsetHariIni = Transaction::where('date', now()->format('Y-m-d'))->sum('total');
-        $omsetHariIniTunai = Transaction::where('date', now()->format('Y-m-d'))->where('payment', 'tunai')->sum('total');
-        $omsetHariIniKredit = Transaction::where('date', now()->format('Y-m-d'))->where('payment', 'kredit')->sum('total');
+        $omsetHariIniTunai = Transaction::where('date', now()->format('Y-m-d'))->where('payment', 'tunai')->sum('pay');
+        $omsetHariIniKredit = Transaction::where('date', now()->format('Y-m-d'))->where('payment', 'kredit')->sum('change') * -1;
 
         #Laba Hari Ini
         $todays = Transaction::join('transaction_details', 'transaction_details.transaction_id', '=', 'transactions.transaction_id')->where('transactions.date', '=', now()->format('Y-m-d'))->get(['transactions.total', 'transaction_details.selling_price', 'transaction_details.purchase_price', 'transaction_details.quantity']);
@@ -35,8 +35,8 @@ class HomeController extends Controller
 
         #Omset Bulan Ini
         $omsetBulanIni = Transaction::whereYear('date', '=', now()->format('Y'))->whereMonth('date', '=', now()->format('m'))->sum('total');
-        $omsetBulanIniTunai = Transaction::whereYear('date', '=', now()->format('Y'))->whereMonth('date', '=', now()->format('m'))->where('payment', 'tunai')->sum('total');
-        $omsetBulanIniKredit = Transaction::whereYear('date', '=', now()->format('Y'))->whereMonth('date', '=', now()->format('m'))->where('payment', 'kredit')->sum('total');
+        $omsetBulanIniTunai = Transaction::whereYear('date', '=', now()->format('Y'))->whereMonth('date', '=', now()->format('m'))->where('payment', 'tunai')->sum('pay');
+        $omsetBulanIniKredit = Transaction::whereYear('date', '=', now()->format('Y'))->whereMonth('date', '=', now()->format('m'))->where('payment', 'kredit')->sum('change') * -1;
 
         #Laba Bulan Ini
         $months = Transaction::join('transaction_details', 'transaction_details.transaction_id', '=', 'transactions.transaction_id')->whereYear('date', '=', now()->format('Y'))->whereMonth('date', '=', now()->format('m'))->get(['transactions.total', 'transaction_details.selling_price', 'transaction_details.purchase_price', 'transaction_details.quantity']);
